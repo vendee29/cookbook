fa63a1b0b06fdda4806f30c143baf6cde8b51a87
Vendy Kudejova <vendula.kudejova@gmail.com>
2023-03-15 15:53:37 +0100

    chore: sets up config for frontend

---
+++ app/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  plugins: [react()],
+});
